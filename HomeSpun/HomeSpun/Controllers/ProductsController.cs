c725eba050007d709fb4ea8b8e02b8d4e027b7a5
riyarosesebastian <36695729+riyarosesebastian@users.noreply.github.com>
2021-05-16 11:24:51 +0530

    Add files via upload

---
+++ HomeSpun/HomeSpun/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSpun.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public ProductsController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = Summaries;
+            return Ok(products);
+
+        }
+    }
+}
